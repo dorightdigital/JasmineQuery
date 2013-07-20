2db89ff2a0d6babaf032fcef7d6d14b8a6eadbab
Mat Carey <mat@matcarey.co.uk>
2013-07-20 16:03:47 +0100

    cleanup of distracted coding

--- test/jasmine/spec/event_mock_spec.js
+++ test/jasmine/spec/event_mock_spec.js
@@ -119,7 +119,6 @@ describe('Event Mocks', function () {
         expect(submitHandler).not.toHaveBeenCalled();
         expect(clickHandler).toHaveBeenCalled();
     });
-    it('should pass through ')
     describe('edge cases and assumptions', function () {
     });
 });
